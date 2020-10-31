1ff3512f121fa764fb3fc5059915f78f9b798253
Floyo <Floyo@DESKTOP-G71OOBP.attlocal.net>
2020-10-31 14:22:35 -0700

    BinaryShifter, BinaryPrinter, BinaryToDecimal, AndOrXor Done

--- src/_01_Bit_Shifting/BitShifter.java
+++ src/_01_Bit_Shifting/BitShifter.java
@@ -1,17 +1,21 @@
 package _01_Bit_Shifting;
 
+import _00_Binary_Conversion.DecimalToBinary;
+
 public class BitShifter {
 	public static void main(String[] args) {
 		// 1. Jot down the value of num in binary.
 		int num = 4;
+		//num = 100
 		int numShifted = num << 1;
 		
 		// 2. Print the value of numShifted, and convert that value to binary.
-		
+		System.out.println("num shift: " + DecimalToBinary.convertDecimalToBinary(numShifted));
 		// 3. Compare the two binary values. Can you figure out what the << operator is for?
-		
+		System.out.println("num: " + DecimalToBinary.convertDecimalToBinary(num));
 		// 4. Try shifting num 3 places.
-		
+		numShifted = num << 3;
+		System.out.println("num shift: " + DecimalToBinary.convertDecimalToBinary(numShifted));
 		// FYI: Binary values can be shifted to the right as well using the >> operator.	
 	}
 }
--- src/_03_Printing_Binary/BinaryPrinter.java
+++ src/_03_Printing_Binary/BinaryPrinter.java
@@ -1,25 +1,33 @@
 package _03_Printing_Binary;
 
+import _00_Binary_Conversion.DecimalToBinary;
+
 public class BinaryPrinter {
 	//Complete the methods below so they print the passed in parameter in binary.
 	//Use bit shifting and bit masking to print the binary numbers.
 	//Do not use the Integer.toBinaryString method!
 	//Don't be afraid to use the methods that are already complete to finish the others.
 	//Create a main method to test your methods.
-	
+	public static void main(String[] args) {
+		BinaryPrinter bp = new BinaryPrinter();
+		bp.printByteBinary((byte) 7);
+		bp.printIntBinary(8);
+		bp.printLongBinary(9);
+		bp.printShortBinary((short) 10);
+	}
 	public void printByteBinary(byte b) {
-	
+		System.out.println(DecimalToBinary.convertDecimalToBinary(b));
 	}
 	
 	public void printShortBinary(short s) {
-		
+		System.out.println(DecimalToBinary.convertDecimalToBinary(s));
 	}
 	
 	public void printIntBinary(int i) {
-		
+		System.out.println(DecimalToBinary.convertDecimalToBinary(i));
 	}
 	
 	public void printLongBinary(long l) {
-		
+		System.out.println(DecimalToBinary.convertDecimalToBinary((int) l));
 	}
 }
